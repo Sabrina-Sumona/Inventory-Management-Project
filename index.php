a895dd3c9816367dba245d2181fc2b918ff48579
Sabrina Sumona <snsbauet04@gmail.com>
2021-03-20 18:40:07 +0600

    welcome page design using html & css

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<html>
+    <head>
+        <title>Welcome</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body scroll="no">
+        <div>
+            <p class="center_align">
+                Welcome to Happy Shop Inventory!
+            </p>
+        </div>
+
+        <div class="col-sm-6">
+            <a href="login.php">
+                <input  type="button" class="btn btn-primary pull-right" value="Login">
+            </a>
+        </div>
+
+        <div class="col-sm-6">
+            <a href="register.php">
+                <input  type="button" class="btn btn-info pull-left" value="Register">
+            </a>
+        </div>
+        <br>
+        <div class="img">
+            <img src="img/welcome.jpg" alt="welcome">
+        </div>
+    </body>
+
+</html>
